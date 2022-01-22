593f490bd602178dfb28efc09157d1e6efff3c82
Chek Wei Chia <chek_wei_chia@aiap.sg>
2022-01-22 16:02:38 +0800

    edited week1 learning notes and HW1 codes

--- Docker_Tutorial/hw1.sql
+++ Docker_Tutorial/hw1.sql
@@ -1,3 +1,5 @@
+-- Below code answers for HW1 for SQL portion
+
 -- question3 (Count records):
 select count(*)
 from yellow_taxi_data
@@ -10,3 +12,20 @@ select tpep_pickup_datetime, tpep_dropoff_datetime, tip_amount from yellow_taxi_
 --question5 (Most popular destination)
 --According to https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv, central park 
 --location id is 43
+select count(*) as total_cnt, "DOLocationID"
+from yellow_taxi_data
+where "PULocationID" = 43
+and 
+tpep_pickup_datetime::date = '2021-01-14'
+group by "DOLocationID"
+order by total_cnt desc
+--Zone for 237 is "Upper East Side South" according to lookup.csv
+--237,"Manhattan","Upper East Side South","Yellow Zone"
+
+--question6 (Most expensive route)
+select "PULocationID", "DOLocationID", avg(total_amount) as avg_total_amt
+from yellow_taxi_data
+group by "PULocationID", "DOLocationID"
+order by avg_total_amt desc
+--4,"Manhattan","Alphabet City","Yellow Zone"
+--265,"Unknown","NA","N/A"
